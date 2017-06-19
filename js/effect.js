7495d33acb3fdfa87ec0894e569ff1e53833cf84
dayfine <difancpa@gmail.com>
2017-06-18 22:06:42 -0400

    way too much effect

--- js/effect.js
+++ js/effect.js
@@ -5,42 +5,33 @@ canvasH = canvas.height = document.body.clientHeight
 var ctx = canvas.getContext('2d')
 ctx.globalCompositeOperation = 'lighter';
 
-var letterDrops = ['0','1','0','1','0','1','0','1','0','1','0','1','0','1','0','1','0','1','0','1','0','1','0','1','0','1','诸','神','集','新','世','夜','明','虎','鸫','啼','花','开','向','神','祈','浮','生','空','自','哀','梦','逝','恨','飘','零']
+var chrDrops =
+'虎鸫悲啼亦如昔,暮然回首百花残,宛似心慰要无踪,诸神集新世,夜明虎鸫啼,花开向神祈,浮生空自哀,梦已逝,在吾生世,悲运缠身,梦已逝,恨飘离'.replace(/,/g,'').split('')
 
-//var letterDrops = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-
-var drops =[];
-for (var i = 0; i < 72; i++) {
-  var newdrop = new Drop()
-  newdrop.arrString = generaText()
-  drops.push(newdrop)
+function Drop(){
+  this.x = -50+Math.floor(Math.random()*(canvasW+100))
+  this.y = Math.floor(Math.random()*-canvasH*.4)
+  this.chrSize = Math.floor(Math.random()*20)+10
+  this.speed = this.chrSize*.5
+  this.arrStr = this.geneText()
 }
 
-
-
-draw();
-
-
-function Drop(whatever){
-  this.x = -50+Math.floor(Math.random()*(canvasW+100));
-  this.y = Math.floor(Math.random()*-canvasH*.4);
-  this.size = Math.floor(Math.random()*8)+16;
-  this.speed = this.size/1.667;
-  this.arrString = [];
-}
-
-function generaText(){
-  let arrString =[]
-  for (var j = 0; j <= Math.random()*17+8; j++) {
-    arrString.push(letterDrops[Math.floor(Math.random()*letterDrops.length)])
+Drop.prototype.geneText = function(){
+  let arrStr =[]
+  for (var j = 0; j <= Math.floor(Math.random()*15)+5; j++) {
+    arrStr.push(chrDrops[Math.floor(Math.random()*chrDrops.length)])
   }
-  return arrString
+  return arrStr
 }
 
-function drawDrop(x, y, size, arrString){
-  ctx.fillStyle='#4AD8F1'
-  for (var i = 0; i < arrString.length; i++) {
-    ctx.fillText(arrString[i], x, y-i*size)
+Drop.prototype.drawDrop = function(context){
+  context.fillStyle='#ffde24'
+  for (var i = 0; i < this.arrStr.length; i++) {
+    context.fillText(
+      this.arrStr[i],
+      this.x,
+      this.y-i*this.chrSize
+    )
   }
 }
 
@@ -48,19 +39,37 @@ function draw() {
   ctx.clearRect(0, 0, canvasW, canvasH)
   ctx.shadowOffsetX = ctx.shadowOffsetY = 0
   ctx.shadowBlur = 10
-  ctx.shadowColor = '#1b8193'
+  ctx.shadowColor = '#ffffff'
 
   for (var k = 0; k < drops.length; k++) {
-    ctx.font = drops[k].size+'px MatrixCode'
+    ctx.font = drops[k].chrSize+'px ZhengBanQiao' //this is definitely too much
     ctx.textBaseline = 'top'
     ctx.textAlign = 'center'
-    if (drops[k].y > canvasH+drops[k].size*drops[k].arrString.length) {
+    if (drops[k].y > canvasH+drops[k].chrSize*drops[k].arrStr.length) {
       drops[k] = new Drop()
       drops[k].y = 0
-      drops[k].arrString = generaText()
-      drawDrop(drops[k].x, drops[k].y, drops[k].size, drops[k].arrString)
-    } else drawDrop(drops[k].x, drops[k].y, drops[k].size, drops[k].arrString)
+      drops[k].drawDrop(ctx)
+    } else {
+      drops[k].drawDrop(ctx)
+    }
     drops[k].y += drops[k].speed
   }
   setTimeout(draw, 100)
 }
+
+
+//Actually Drawing!!!
+var drops =[];
+for (var i = 0; i < 100; i++) {
+  drops.push(new Drop())
+}
+draw();
+
+
+
+
+
+
+
+
+
